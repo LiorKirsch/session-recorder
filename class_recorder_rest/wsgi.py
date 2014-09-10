205db81cefadfe3c72d559c7d584c7d4cbe7f711
liorkirsch <liorkirsch@web307.webfaction.com>
2014-09-10 08:42:59 +0000

    somechanges to files

--- class_recorder_rest/wsgi.py
+++ class_recorder_rest/wsgi.py
@@ -1,14 +1,8 @@
-"""
-WSGI config for class_recorder_rest project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "class_recorder_rest.settings")
+import sys
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+os.environ['DJANGO_SETTINGS_MODULE'] = 'class_recorder_rest.settings'
+
+from django.core.handlers.wsgi import WSGIHandler
+
+application = WSGIHandler()
